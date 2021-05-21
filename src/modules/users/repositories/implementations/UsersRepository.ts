4e25b88b0297090c33661364e599e88ecbcdec66
Artur Noura Teixeira <artur.teixeira@serpro.gov.br>
2021-05-20 22:27:03 -0300

    OK

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,34 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({ id: user_id }, {
+      relations: ['games'],
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(
+      'SELECT id, first_name, last_name, email, created_at, updated_at' +
+      ' FROM users' +
+      ' ORDER BY first_name ASC'
+    );
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT id, first_name, last_name, email, created_at, updated_at` +
+      ` FROM users` +
+      ` WHERE LOWER(first_name) = LOWER(\'${first_name}\')` +
+      ` AND LOWER(last_name) = LOWER(\'${last_name}\')`
+    );
   }
 }
